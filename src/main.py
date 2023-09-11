1453a027598e6382997deded975a3ecbfdf3b7f8
David Arevalo <david.arevalo@latana.com>
2023-09-11 10:15:18 +0200

    updates

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import json
 def lambda_handler(event, _):
     # Introduce Lambda logic
     
-    # lambda_version = "stable version v_2023_08"
+    lambda_version = "stable version v_2023_08"
+    stage = event["stageVariables"]["lambdaAlias"]
+
     # one comment
-    lambda_version = "dev version"
+    # lambda_version = "dev version runing tests"
 
     return {
         'statusCode': 200,
